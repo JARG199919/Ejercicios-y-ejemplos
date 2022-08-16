340ef7550ec452cf0a6d0c6a206800324133431d
JARG199919 <00370321@uca.edu.sv>
2022-08-15 19:27:03 -0600

    optimizacion y agregamientos de decimales

--- Ejercicio-1-semana-1/Ejercicio1/ejerc1.2/prueba1.2.cpp
+++ Ejercicio-1-semana-1/Ejercicio1/ejerc1.2/prueba1.2.cpp
@@ -2,20 +2,33 @@
 #include <stdlib.h>
 #include <iostream>
 
+//Calculadora sencilla de dos cifras
+
+//Se tratara de agregar array para poder agregar mas numeros para procesamiento
+
 //variables global
-int numero1=0, numero2=0,resultado=0, num;
+float numero1=0, numero2=0,resultado=0;
+int num, repiter=1;
 
 //Funciones
 void entradaDatos(),proceso2(), inicio();
+
+//programa principal
 int main(){
     system("cls");
+    while (repiter==1){
     inicio();
     entradaDatos();
     proceso2();
-    printf("la respuesta es: %d",resultado);
-    return 0;
+    printf("la respuesta es: %.2f\n\n1.Volver al inicio\n2.Salir\n",resultado);
+    scanf("%d",&repiter);
+    system("cls");
+    }
 }
+
 // Definiciones de funciones
+
+//Pantalla de informacion
 void inicio(){ 
 printf("Ingrese un numero\n 1.suma \n 2.resta \n 3.Multiplicacion \n 4.Division\n");
    scanf("%d",&num);
@@ -25,9 +38,10 @@ printf("Ingrese un numero\n 1.suma \n 2.resta \n 3.Multiplicacion \n 4.Division\
        scanf("%d",&num);
    }
 }
+//Obtencion de datos de entrada
 void entradaDatos(){
     system("cls");
-    printf("Se ha selecionado ");
+    printf("Se ha selecionado, ");
     switch (num){
     case 1:printf("Suma\n");
         break;
@@ -39,10 +53,11 @@ void entradaDatos(){
         break;
     }
     printf("Ingrese el primer numero: ");
-    scanf("%d",&numero1);
+    scanf("%f",&numero1);
     printf("Ingrese el segundo numero: ");
-    scanf("%d",&numero2);
+    scanf("%f",&numero2);
 }
+//Procesamineto de datos
 void proceso2(){
     switch (num){
     case 1:resultado=numero1+numero2;
