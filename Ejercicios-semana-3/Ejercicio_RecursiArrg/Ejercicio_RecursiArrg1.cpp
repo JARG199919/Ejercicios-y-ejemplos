76acafdbeb67259bf598f10324d861c254cef67f
JARG199919 <00370321@uca.edu.sv>
2022-09-06 18:47:45 -0600

    se agrego un menu

--- Ejercicios-semana-3/Ejercicio_RecursiArrg/Ejercicio_RecursiArrg1.cpp
+++ Ejercicios-semana-3/Ejercicio_RecursiArrg/Ejercicio_RecursiArrg1.cpp
@@ -4,14 +4,15 @@
 
 using namespace std;
 
-int **arreglo, numIN,numeroMenu=0;
+int **arreglo, numIN,numeroMenu=0,numFilas, numColumnas;
 
 int factorial(int);
+void arreglosDinamicos();
 
 int main()
 {
 
-    cout << "Opciones: \n1.Factorial !\n2.Ingresar " << endl;
+    cout << "Opciones: \n1.Factorial !\n2.Mostrar una matriz " << endl;
     cin >> numeroMenu;
     
 
@@ -23,6 +24,8 @@ int main()
         cout << "\nSolucion Recursiva: " << factorial(numIN) << endl;
         break;
     case 2:
+
+        arreglosDinamicos();
         break;
 
     default:
@@ -50,4 +53,48 @@ int factorial(int n)
         cout << n << " x ";
     // Se realiza el llamado a la función nuevamente
     return n * factorial(n - 1);
+}
+void arreglosDinamicos(){
+    
+    cout << "Ingresa el numero de filas: ";
+    cin >> numFilas;
+    cout << "Ingresa el numero de columnas: ";
+    cin >> numColumnas;
+    // Reserva de memoria
+    arreglo = new int *[numFilas];
+    for (int i = 0; i < numFilas; i++)
+    {
+        arreglo[i] = new int[numColumnas];
+    }
+    //Capturar los datos 
+    for (int i = 0; i < numFilas; i++)
+    {
+        for (int j = 0; j < numColumnas; j++)
+        {
+            //cout<<"Ingresa valores de filas y columnas";
+            //cin>>*(*(arreglo+i)+j);
+            arreglo[i][j]=1 + rand() %9;
+        }
+    //Agregar un multiplicador,un promedio, suma, y ordenamiento
+    
+    //Mostrar datos
+        for (int i = 0; i < numFilas; i++)
+    {
+        for (int j = 0; j < numColumnas; j++)
+        { 
+            cout<<"|"<<*(*(arreglo+i)+j)<<"| ";
+        }
+        cout<<endl;
+    }
+
+    //Liberacion de memoria
+    for (int i = 0; i < numFilas; i++)
+    {
+        for (int j = 0; j < numColumnas; j++)
+        {
+            delete[]arreglo[i];
+        }
+        
+    }
+        delete[]arreglo;
 }
