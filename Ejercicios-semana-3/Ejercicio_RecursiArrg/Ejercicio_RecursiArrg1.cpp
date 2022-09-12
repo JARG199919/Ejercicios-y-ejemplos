3b8c3754d5f2222679f797c3317ff5e3e85694e8
JARG199919 <00370321@uca.edu.sv>
2022-09-12 16:12:38 -0600

    Se agrego un menu y nuevas funciones

--- Ejercicios-semana-3/Ejercicio_RecursiArrg/Ejercicio_RecursiArrg1.cpp
+++ Ejercicios-semana-3/Ejercicio_RecursiArrg/Ejercicio_RecursiArrg1.cpp
@@ -1,44 +1,49 @@
 #include <iostream>
 #include <stdio.h>
 
-
 using namespace std;
 
-int **arreglo, numIN,numeroMenu=0,numFilas, numColumnas;
-
+int **arreglo, numIN, numeroMenu = 0, numFilas, numColumnas, num, inpp, aux, contar = 0, minimoNum, *arreglo1;
 int factorial(int);
 void arreglosDinamicos();
+void arreglosOrdenamiento();
 
 int main()
 {
-
-    cout << "Opciones: \n1.Factorial !\n2.Mostrar una matriz " << endl;
-    cin >> numeroMenu;
-    
-
-    switch (numeroMenu)
+    do
     {
-    case 1:
-        cout << "Eleccion de Factorial \nIngrese la inicializacion: " << endl;
-        cin >> numIN;
-        cout << "\nSolucion Recursiva: " << factorial(numIN) << endl;
-        break;
-    case 2:
+        system("cls");
 
-        arreglosDinamicos();
-        break;
+        cout << "Opciones: \n1.Factorial !\n2.Mostrar una matriz\n3.Ordenamiento de numeros" << endl;
+        cin >> numeroMenu;
+        switch (numeroMenu)
+        {
+        case 1:
 
-    default:
-        cout << "Ingrese un numero Valido "<<endl;
-       break;
-    
-    }
+            cout << "Eleccion de Factorial \nIngrese la inicializacion: " << endl;
+            cin >> numIN;
+            cout << "\nSolucion Recursiva: " << factorial(numIN) << endl;
+            return 0;
+        case 2:
+            cout << "Eleccion de Tabla :" << endl;
+            arreglosDinamicos();
+            break;
 
-   return 0;
-   
+        case 3:
+            arreglo1 = new int[num];
+            cout << "Eleccion de Ordenamiento" << endl;
+            arreglosOrdenamiento();
+            break;
+        default:
+            cout << "Ingrese un numero Valido " << endl;
+            break;
+        }
+    } while (numeroMenu != 3);
+    return 0;
 }
 
 // Recursiva
+
 int factorial(int n)
 { // cálculo de n!
     // Condición de salida
@@ -54,8 +59,9 @@ int factorial(int n)
     // Se realiza el llamado a la función nuevamente
     return n * factorial(n - 1);
 }
-void arreglosDinamicos(){
-    
+void arreglosDinamicos()
+{
+
     cout << "Ingresa el numero de filas: ";
     cin >> numFilas;
     cout << "Ingresa el numero de columnas: ";
@@ -66,35 +72,113 @@ void arreglosDinamicos(){
     {
         arreglo[i] = new int[numColumnas];
     }
-    //Capturar los datos 
+    // Capturar los datos
     for (int i = 0; i < numFilas; i++)
     {
         for (int j = 0; j < numColumnas; j++)
         {
-            //cout<<"Ingresa valores de filas y columnas";
-            //cin>>*(*(arreglo+i)+j);
-            arreglo[i][j]=1 + rand() %9;
+            // cout<<"Ingresa valores de filas y columnas";
+            // cin>>*(*(arreglo+i)+j);
+            arreglo[i][j] = 1 + rand() % 9;
         }
-    //Agregar un multiplicador,un promedio, suma, y ordenamiento
-    
-    //Mostrar datos
-        for (int i = 0; i < numFilas; i++)
+    } // Agregar un multiplicador,un promedio, suma, y ordenamiento
+
+    // Mostrar datos
+    for (int i = 0; i < numFilas; i++)
     {
         for (int j = 0; j < numColumnas; j++)
-        { 
-            cout<<"|"<<*(*(arreglo+i)+j)<<"| ";
+        {
+            cout << "|" << *(*(arreglo + i) + j) << "|";
         }
-        cout<<endl;
+        cout << endl;
     }
 
-    //Liberacion de memoria
+    // Liberacion de memoria
     for (int i = 0; i < numFilas; i++)
     {
         for (int j = 0; j < numColumnas; j++)
         {
-            delete[]arreglo[i];
+            delete[] arreglo[i];
         }
-        
     }
-        delete[]arreglo;
+    delete[] arreglo;
+}
+void arreglosOrdenamiento()
+{
+    system("cls");
+    cout << "Ingrese el tamano del arreglo (Max 9)\n";
+    cin >> num;
+    if (num > 10 || num < 1)
+    {
+        cin >> num;
+    }
+    do
+    {
+        system("cls");
+        cout << "Los datos de el arreglo desea\n1.ingresarlos\n2.Aleatorios\n";
+        cin >> inpp;
+        switch (inpp)
+        {
+        case 1:
+            system("cls");
+            for (int i = 0; i < num; i++)
+            {
+                contar++;
+                printf("Ingrese el %d de la cantidad:", contar);
+                cin >> arreglo1[i];
+            }
+
+            break;
+
+        case 2:
+            int max, min;
+            system("cls");
+            cout << "Ingrese el valor minimo (limite -200k digitos):";
+            cin >> min;
+            system("cls");
+            cout << "Ingrese el valor maximo proximado(limite 200k digitos):";
+            cin >> max;
+            system("cls");
+            cout << "El valor aleatorio se escogio: \n";
+            for (int i = 0; i < num; i++)
+            {
+                arreglo1[i] = min + rand() % max;
+                cout << arreglo1[i] << " ";
+            }
+
+            break;
+
+        default:
+            system("cls");
+            cout << "Ingrese un numero valido"<<endl;
+            system("PAUSE");
+            
+        }
+    }while (inpp != 2); 
+
+    for (int i = 0; i < num; i++)
+    {
+        // Hipotesis, que el primer valor del arreglo es el menor
+        minimoNum = i;
+        // Segundo recorrido se inicia en la segunda posición
+        for (int j = i + 1; j < num; j++)
+        {
+            // Comparación entre elemento actual con el primer valor que se estableció como menor
+            if (arreglo1[j] < arreglo1[minimoNum])
+            {
+                // Establecer el elemento actual como el menor valor
+                minimoNum = j;
+            }
+        }
+        // Intercambio de valores del arreglo
+        aux = arreglo1[i];
+        arreglo1[i] = arreglo1[minimoNum];
+        arreglo1[minimoNum] = aux;
+    }
+
+    cout << "\nEl orden final en modo seleccion es: \n";
+    for (int i = 0; i < num; i++)
+    {
+        cout << arreglo1[i] << " ";
+    }
 }
